5d3d88e5165b39bd4cb515d7f92991e278fd4791
ozz1979 <info@diareklamajans.com>
2026-05-15 16:52:14 +0300

    v7.2: baslat.bat

--- baslat.bat
+++ baslat.bat
@@ -1,6 +1,6 @@
 @echo off
 chcp 65001 >nul 2>&1
-title Sesli AI Asistan v7.1
+title Sesli AI Asistan v7.0
 cd /d "%~dp0"
 if not exist "venv\Scripts\python.exe" (
     echo [HATA] Sanal ortam bulunamadi! Once kur.bat calistirin.
