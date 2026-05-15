1096dc6ee4c98478930136dde472a10ebb35d922
ozz1979 <info@diareklamajans.com>
2026-05-15 17:49:03 +0300

    v7.5

--- hafiza.py
+++ hafiza.py
@@ -1,5 +1,5 @@
 """
-Hafiza Modulu v7.4
+Hafiza Modulu v7.5
 - Kullanici tanima (isim kayit)
 - Tercihler, rutinler ve ogrenme
 - Yapilandirilmis hafiza ozeti (AI context icin optimize)
@@ -20,7 +20,7 @@ class Hafiza:
             try:
                 with open(self.dosya, "r", encoding="utf-8") as f:
                     veri = json.load(f)
-                    # v7.4 alanlari yoksa ekle
+                    # v7.5 alanlari yoksa ekle
                     if "kullanici" not in veri:
                         veri["kullanici"] = {"adi": "", "ilk_kullanim": ""}
                     return veri
